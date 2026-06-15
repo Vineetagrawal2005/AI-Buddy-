61d272b5fe69c09f3adf2dd4b5f21091334a621e
Vineetagrawal2306 <vineetagrawal2306@gmail.com>
2026-06-15 15:03:29 +0530

    AI Buddy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,9 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
-        versionName = flutter.versionName
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"  
     }
 
     buildTypes {
